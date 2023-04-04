ad8833de6d8a3872b259dd42135fb51864b2362f
shanialenihan <shanialenihan@gmail.com>
2023-04-04 05:54:07 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# L07HandsOnAssignment
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-pjp3nd)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Lesson 7 HandsOn</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
